4cdb08a71b8c2588ca7dc7a7bbd30f84c4573bee
Homculus <idogbegah@gmail.com>
2017-06-19 19:34:41 +0200

    Mostly concerns CSS and HTML
    Why doesn't the fucking navbar work?

--- JavaScript.js
+++ JavaScript.js
@@ -1,4 +1,13 @@
-﻿var hours = Date();
+﻿document.addEventListener("DOMContentLoaded", function (event)
+{
+    var fullDate = new Date();
+    var hours = fullDate.getHours();
+    var minutes = fullDate.getMinutes();
+    var seconds = fullDate.getSeconds();
+    var time = fullDate.getTime();
+    // document.getElementById("time").innerHTML = hours;
 
-document.getElementById("time").innerHTML = "Work.";
+});
+
+
 
